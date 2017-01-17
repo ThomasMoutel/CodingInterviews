651ce71aedfdaac07823680de99db6a846034be6
thomas.moutel <thomas.moutel@thomsonreuters.com>
2017-01-17 18:14:45 +0100

    Trying something better!

--- Diabolo/Angular/app/app.js
+++ Diabolo/Angular/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var gameStartedTick = new Date();
+var gameStartedTick;
 var numberToFind;
 var numberOfTries = 0;
 
@@ -13,6 +13,9 @@ var app = angular.module('GuessTheNumber', []);
 
 app.controller("GuessController", function($scope) {
   $scope.checkInput = function() {
+    if (numberOfTries == 0) {
+      gameStartedTick = new Date();
+    }
     numberOfTries = numberOfTries + 1;
     if ($scope.GuessInput == numberToFind) {
       var gameLength = Math.floor((new Date() - gameStartedTick) / 1000);
@@ -34,13 +37,22 @@ app.controller("GuessController", function($scope) {
       if ($scope.previousGuesses.indexOf(guessToRemember) == -1) {
         $scope.previousGuesses.unshift(guessToRemember);
       }
+      $scope.previousGuesses2.unshift({ 'value': $scope.GuessInput, 'error': result });
+      // console.debug($scope.previousGuesses2);
     }
   }
   $scope.previousGuesses = [];
+  $scope.previousGuesses2 = [];
 });
 
 app.directive("previousGuesses", function() {
   return {
     template: '<ul> <li ng-repeat="p in previousGuesses"> {{ p }}'
-  };
+  }
+});
+
+app.directive("myGuessDisplay", function() {
+  return {
+    templateUrl: 'guess.html'
+  }
 });
